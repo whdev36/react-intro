9b2ff23127cad5d5e7365197e645fc01a091bf01
whdev36 <wisdomhunter36@gmail.com>
2024-11-29 10:17:10 -0800

    condition rendering

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -4,11 +4,30 @@ import ListGroup from "./components/ListGroup";
 import { Fragment } from "react";
 
 function App() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+
+  // if (items.length === 0)
+  //   return (
+  //     <Fragment>
+  //       <h1>List</h1>
+  //       <p>No item found</p>
+  //     </Fragment>
+  //   );
+
+  // const message = items.length === 0 ? <p>No item found</p> : null;
+
+  // const getMessage = () => {
+  //   return items.length === 0 ? <p>No item found</p> : null;
+  // }
 
   return (
     <Fragment>
       <h1>List</h1>
+      {items.length === 0 ? <p>No item found</p> : null}
+      {/* {message} */}
+      {/* { getMessage() } */}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
