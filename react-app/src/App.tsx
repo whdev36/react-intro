608cfe5e638112b721e9fddd511a4629fcb175d7
whdev36 <wisdomhunter36@gmail.com>
2024-11-29 09:56:54 -0800

    map

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,18 +1,18 @@
 // import Message from './Message';
 import ListGroup from "./components/ListGroup";
 
-import { Fragment } from 'react';
+import { Fragment } from "react";
 
 function App() {
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
   return (
     <Fragment>
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </Fragment>
   );
