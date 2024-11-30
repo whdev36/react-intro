b003a326bba5bd849969e7dc150ae181a7250b14
whdev36 <wisdomhunter36@gmail.com>
2024-11-30 12:36:20 -0800

    mouse event handling

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,36 +1,21 @@
-// import Message from './Message';
 import ListGroup from "./components/ListGroup";
-
 import { Fragment } from "react";
+import { MouseEvent } from "react";
 
 function App() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
 
-  // if (items.length === 0)
-  //   return (
-  //     <Fragment>
-  //       <h1>List</h1>
-  //       <p>No item found</p>
-  //     </Fragment>
-  //   );
-
-  // const message = items.length === 0 ? <p>No item found</p> : null;
-
-  // const getMessage = () => {
-  //   return items.length === 0 ? <p>No item found</p> : null;
-  // }
+  const handleClick = (e: MouseEvent) => console.log(e);
 
   return (
     <Fragment>
       <h1>List</h1>
-      {items.length === 0 ? <p>No item found</p> : null}
-      {/* {message} */}
-      {/* { getMessage() } */}
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li className="list-group-item" onClick={handleClick} key={item}>
+            {item}
+          </li>
         ))}
       </ul>
     </Fragment>
