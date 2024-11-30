f6a31352400ad679d706e67c14acc6ca1ab81f87
whdev36 <wisdomhunter36@gmail.com>
2024-11-30 13:23:10 -0800

    props functions

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,7 +2,14 @@ import ListGroup from "./components/ListGroup";
 
 function App() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  return <div><ListGroup items={items} heading="Cities"/></div>
+  const handSelectItem = (item: string) => {
+    console.log(item);
+  };
+  return (
+    <div>
+      <ListGroup items={items} heading="Cities" onSelectItem={handSelectItem} />
+    </div>
+  );
 }
 
 export default App;
--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -3,9 +3,10 @@ import { Fragment, useState } from "react";
 interface Props {
   items: string[];
   heading: string;
+  onSelectItem: (item: string) => void;
 }
 
-function ListGroup({items, heading}: Props) {
+function ListGroup({items, heading, onSelectItem}: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
@@ -22,6 +23,7 @@ function ListGroup({items, heading}: Props) {
             }
             onClick={() => {
               setSelectedIndex(index);
+              onSelectItem(item);
             }}
             key={item}
           >
